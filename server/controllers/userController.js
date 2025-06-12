d1c69772aa8e3d80cbff417cda99568be8a924d5
Med-OmarAyach <mohamedomar.hadjayach@ensi-uma.tn>
2025-06-12 14:05:22 +0100

    reservation functions and jwt auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,12 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const token = jwt.sign(
+  { userId: user._id, username: user.username }, // payload
+  process.env.JWT_SECRET,                         // secret
+  { expiresIn: process.env.JWT_EXPIRES_IN }      // options
+);
 
 const createUser = async (req, res) => {
     
@@ -26,4 +33,22 @@ const createUser = async (req, res) => {
 
 
 module.exports = { createUser };
-
+const loginUser = async (req, res) => {
+    try {
+        const { name, password } = req.body;
+        const user = await User.findOne({ username: name });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid username or password" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid username or password" });
+        }
+        res.status(200).json({ message: "Login successful", user,token: token });
+    }
+    catch (error) {
+        console.error("Error logging in user:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+module.exports = {loginUser };

2af0ee8c7a92eeb688db273aaba18e242912b867
ghassenamich <ghassenamichey@gmail.com>
2025-06-13 16:50:41 +0100

    modified:   server/models/Reservation.js

--- server/models/Reservation.js
+++ server/models/Reservation.js
@@ -4,7 +4,9 @@ const reservationSchema = new mongoose.Schema({
   tableId: {type: mongoose.Schema.Types.ObjectId,ref: 'Table',required: true},
   startTime: {type: Date,required: true},
   durationMinutes: {type: Number,required: true,min: 15},
-  userId: {type: mongoose.Schema.Types.ObjectId,ref: 'User',required: false}
+  userId: {type: mongoose.Schema.Types.ObjectId,ref: 'User',required: false},
+  status: {
+    type: String,enum: ['pending', 'confirmed', 'cancelled'],default: 'pending',required: true},
 },
 {
   timestamps: true
